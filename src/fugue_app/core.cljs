2f69b46b0a24fa48b744c0b799f07fdfd7dbd47b
Philip Del Vecchio <delphi@seas.upenn.edu>
2016-04-09 17:28:51 -0400

    Set up editor

--- src/fugue_app/core.cljs
+++ src/fugue_app/core.cljs
@@ -1,6 +1,7 @@
 (ns fugue-app.core
   (:require [cljs.tools.reader :refer [read-string]]
-            [cljs.js :refer [empty-state eval js-eval]]))
+            [cljs.js :refer [empty-state eval js-eval]]
+            [goog.dom :refer [getElement]]))
 
 (defn eval-str [s]
   (eval (empty-state)
@@ -14,4 +15,9 @@
   (js/console.log "Test1"))
 
 (defn ^:export main []
-  (eval-str "(test1)"))
+  (js/CodeMirror.
+   (getElement "editor")
+   (js-obj
+    "theme" "base16-ocean"
+    "mode" "clojure"
+    "lineNumbers" false)))
