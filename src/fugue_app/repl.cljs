3e96efb1385ceba6f7bb78b6f7ff2730bee891a7
Philip Del Vecchio <delphi@seas.upenn.edu>
2016-04-10 15:35:09 -0400

    sin 440

--- src/fugue_app/repl.cljs
+++ src/fugue_app/repl.cljs
@@ -1,5 +1,8 @@
 (ns fugue-app.repl
-  (:require [replumb.core :as replumb]))
+  (:require [replumb.core :as replumb]
+            [fugue-app.io :as io]))
+
+(defonce repl-opts (replumb/options :browser ["cljs" "js"] io/fetch-file!))
 
 (defn clear-marks [c]
   (doseq [mark (.getAllMarks c)]
@@ -43,6 +46,13 @@
       freeze
       set-cursor-to-end))
 
+(defn user-input
+  "Reads the input string from the console"
+  [c]
+  (let [pred #(when (= "repl-buffer" (.-className %)) (.find %))
+        from (.-to (some pred (-> c .getDoc .getAllMarks)))]
+    (.getRange (.getDoc c) from (end-pos c))))
+
 (defn handle-result!
   [c result]
   (if (replumb/success? result)
@@ -53,12 +63,10 @@
 (defn read-eval-print-prompt!
   "TODO: Check if unmatched parens"
   [c]
-  (let [pred #(when (= "repl-buffer" (.-className %)) (.find %))
-        from (.-to (some pred (-> c .getDoc .getAllMarks)))
-        to (end-pos c)
-        user-input (.getRange (.getDoc c) from to)]
-    (writeln! c)
-    (replumb/read-eval-call (partial handle-result! c) user-input)))
+  (writeln! c)
+  (replumb/read-eval-call repl-opts
+                          (partial handle-result! c)
+                          (user-input c)))
 
 (defn start-repl!
   "Starts a fugue repl in the input CodeMirror"
